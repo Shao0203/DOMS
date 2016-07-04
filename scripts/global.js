137e3f896ab9999d35290dbc48036da09021008a
Bright <nzsyl0203@gmail.com>
2016-07-04 23:34:33 +1200

    test functions
    test functions

--- scripts/global.js
+++ scripts/global.js
@@ -1,34 +1,34 @@
-function addLoadEvent(func) {
-	var oldonload = window.onload;
-	if (typeof window.onload != 'function') {
-		window.onload = func;
-	} else {
-		window.onload = function() {
-			oldonload();
-			func();
-		}
-	}
-}
+// function addLoadEvent(func) {
+// 	var oldonload = window.onload;
+// 	if (typeof window.onload != 'function') {
+// 		window.onload = func;
+// 	} else {
+// 		window.onload = function() {
+// 			oldonload();
+// 			func();
+// 		}
+// 	}
+// }
 
-function insertAfter(newElement,targetElement){
-	var parent = targetElement.parentNode;
-	if (targetElement == parent.lastChild) {
-		parent.appendChild(newElement);
-	} else {
-		parent.insertBefore(newElement,targetElement.nextSibling);
-	}
-}
+// function insertAfter(newElement,targetElement){
+// 	var parent = targetElement.parentNode;
+// 	if (targetElement == parent.lastChild) {
+// 		parent.appendChild(newElement);
+// 	} else {
+// 		parent.insertBefore(newElement,targetElement.nextSibling);
+// 	}
+// }
 
-function addClass(element,value) {
-	if (!element.className) {
-		element.classNme = value;
-	} else {
-		newClassName = element.className;
-		newClassName+= ' ';
-		newClassName+= value;
-		element.className = newClassName;
-	}
-}
+// function addClass(element,value) {
+// 	if (!element.className) {
+// 		element.classNme = value;
+// 	} else {
+// 		newClassName = element.className;
+// 		newClassName+= ' ';
+// 		newClassName+= value;
+// 		element.className = newClassName;
+// 	}
+// }
 
 function highlightPage() {
 	if (!document.getElementById) return false;

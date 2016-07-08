e973ed8747d7910a83e68a00b4b05d8eccf3f7f2
Bright <nzsyl0203@gmail.com>
2016-07-08 22:13:16 +1200

    function displayAbbr
    function displayAbbr

--- scripts/global.js
+++ scripts/global.js
@@ -231,6 +231,38 @@ function highlightRows() {
 	}
 }
 
+function displayAbbr() {
+	var abbreviations = document.getElementsByTagName('abbr');
+	if (abbreviations.length == 0) return false;
+	var defs = new Array();
+	for (var i = 0; i < abbreviations.length; i++) {
+		var current_abbr = abbreviations[i];
+		if (current_abbr.childNodes.length < 1) continue;
+		var definition = current_abbr.getAttribute('title');
+		var key = current_abbr.lastChild.nodeValue;
+		defs[key] = definition;
+	}
+	var dlist = document.createElement('dl');
+	for (key in defs) {
+		var definition = defs[key];
+		var dtitle = document.createElement('dt');
+		var dtitle_text = document.createTextNode(key);
+		dtitle.appendChild(dtitle_text);
+		var ddesc = document.createElement('dd');
+		var ddesc_text = document.createTextNode(definition);
+		ddesc.appendChild(ddesc_text);
+		dlist.appendChild(dtitle);
+		dlist.appendChild(ddesc);
+	}
+	if (dlist.childNodes.length == 0) return false;
+	var header = document.createElement('h3');
+	var header_text = document.createTextNode('Abbreviations');
+	header.appendChild(header_text);
+	var articles = document.getElementsByTagName('article');
+	if (articles.length == 0) return false;
+	articles[0].appendChild(header);
+	articles[0].appendChild(dlist);
+}
 
 addLoadEvent(highlightPage);
 addLoadEvent(prepareSlideshow);
@@ -239,6 +271,7 @@ addLoadEvent(preparePlaceholder);
 addLoadEvent(gallery);
 addLoadEvent(stripeTables);
 addLoadEvent(highlightRows);
+addLoadEvent(displayAbbr);
 
 
 
